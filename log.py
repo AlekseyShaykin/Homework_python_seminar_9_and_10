56304d67f7fdc6325f7e887688f0ebe222343fe4
AlekseyShaykin <alekseyshaykin2311@gmail.com>
2023-02-02 21:08:09 +0700

    Bot updated

--- log.py
+++ log.py
@@ -4,6 +4,6 @@ import datetime
 
 
 async def log(update: Update, context: ContextTypes.DEFAULT_TYPE):
-   file = open('PYTHON\Seminar_10\db.csv', 'a')
+   file = open('PYTHON\Seminar_10\db.csv', 'a', encoding = 'utf-8')
    file.write(f'{update.effective_user.first_name},{update.effective_user.id},{update.message.text} {datetime.datetime.now()}\n')
    file.close()
